cf0ca611672324f31aac136e8e41f1b72d243a8e
MagicCoderSigma <fauazcarraga22@src.pshs.edu.ph>
2025-01-22 11:11:42 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const pi = 3.14;
-let radius = 6.378e+10;
+let radius;
+radius = 6.378e+10;
 let circumference = 2 * pi * radius;
 let surfaceArea = 4 * pi * (radius ** 2);
 let volume = 4 / 3 * pi * (radius ** 3);
 
-document.write("The radius of the sphere is " + radius + ".");
-document.write("<br>The circumference of the sphere is " + circumference + ".");
-document.write("<br>The surface area of the sphere is " + surfaceArea + ".");
-document.write("<br>The volume of the sphere is " + volume + ".");
+document.write("The radius of the sphere is: " + radius + ".");
+document.write("<br>The circumference of the sphere is: " + circumference + ".");
+document.write("<br>The surface area of the sphere is: " + surfaceArea + ".");
+document.write("<br>The volume of the sphere is: " + volume + ".");
